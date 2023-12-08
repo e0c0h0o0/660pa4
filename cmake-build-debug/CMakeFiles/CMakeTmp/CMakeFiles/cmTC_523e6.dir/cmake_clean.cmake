c51a5e5bf9be1dbdae082d5be39b31ebf2c94a9e
e0c0h0o0 <146396698+e0c0h0o0@users.noreply.github.com>
2023-12-08 04:21:36 -0500

    pa4
    pa4

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_523e6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_523e6.dir/CMakeCXXCompilerABI.cpp.o"
+  "cmTC_523e6"
+  "cmTC_523e6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_523e6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
